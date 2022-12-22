c7044e2d0152319390f3e731c7ec62da4ea30288
ianniphd <10ianni@gmail.com>
2022-12-23 00:25:50 +0100

    add figures

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.inspection import DecisionBoundaryDisplay
 import matplotlib.colors
+import os
 
 def monoplot(
         link="https://raw.githubusercontent.com/ianni-phd/Datasets/main/2022-11-12_calciatori_marcatura.csv",
         target = "Classe",
         features = ["Passaggio", "Marcatura", "Tiro", "Tecnica"],
-        pair=["Tiro", "Marcatura"]
-        # Tiro
+        pair=["Tiro", "Marcatura"],
+        min_samples_leaf = 2,
+        max_depth = 999
 ):
+    title = "min_sample_leaf: {}, max_depth: {}".format(min_samples_leaf, max_depth)
+    print(title)
     df = pd.read_csv(link, sep=';', decimal=',')
 
     # Make it binary
@@ -37,9 +41,10 @@ def monoplot(
     #X_2_cols
 
     # MODELING
-    clf = DecisionTreeClassifier()                                  # PRIMA PROVA
-    # clf = DecisionTreeClassifier(min_samples_leaf=2)              # SECONDA PROVA
-    clf = DecisionTreeClassifier(min_samples_leaf=2, max_depth=4)   # TERZA PROVA
+    # clf = DecisionTreeClassifier(min_samples_leaf=0, max_depth=999)   # PRIMA PROVA
+    # clf = DecisionTreeClassifier(min_samples_leaf=2, max_depth=999)   # SECONDA PROVA
+    # clf = DecisionTreeClassifier(min_samples_leaf=2, max_depth=4)     # TERZA PROVA
+    clf = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf, max_depth=max_depth)
     clf.fit(X_2_cols, y)
     y_prev = clf.predict(X_2_cols)
     accuracy = accuracy_score(y_prev, y)
@@ -57,7 +62,8 @@ def monoplot(
     plot_colors = ["ygb" if n_classes == 3 else "yb"][0]
     # plot_step = 0.02
 
-    # plt.figure(figsize=(22,6))
+    plt.figure(figsize=(22,6))
+    plt.title(title)
     DecisionBoundaryDisplay.from_estimator(
         clf,
         X_2_cols,
@@ -66,17 +72,21 @@ def monoplot(
         xlabel=pair[0],
         ylabel=pair[1],
     )
+    plt.savefig(os.path.join(os.getcwd(), "output", title+"_Boundary.png"))
 
     fig = plt.figure(figsize=(25, 20))
+    plt.title(title)
     _ = tree.plot_tree(clf, feature_names=["Shot", "Marking"], class_names="Class", filled=True)
-
+    plt.savefig(os.path.join(os.getcwd(),"output",title+"_Tree.png"))
 
 
 def multiplot(
         link = "https://raw.githubusercontent.com/ianni-phd/Datasets/main/2022-11-12_calciatori_marcatura.csv",
         target = "Classe",
         features = ["Passaggio", "Marcatura", "Tiro", "Tecnica"],
-        dict_2_classes = {"D": "other", "C": "other", "A": "strickers"}
+        dict_2_classes = {"D": "other", "C": "other", "A": "strickers"},
+        pairs_to_focus=[["Tiro", "Passaggio"], ["Tiro", "Tecnica"], ["Tiro", "Simpatia"],
+                        ["Tiro", "Marcatura"], ["Marcatura", "Passaggio"], ["Tecnica", "Marcatura"]]
 ):
 
     cmap_yg_gr_lb = matplotlib.colors.ListedColormap(["yellow", "green", "lightblue"])
@@ -90,8 +100,6 @@ def multiplot(
     df[target] = pd.factorize(df[target])[0]
     n_classes = len(classes_labels)
 
-
-
     X = df[features].values
     y = df[target].values
 
@@ -103,8 +111,6 @@ def multiplot(
     fig.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
 
     # deciding the features to compare
-    pairs_to_focus = [["Tiro", "Passaggio"], ["Tiro", "Tecnica"], ["Tiro", "Simpatia"],
-                      ["Tiro", "Marcatura"], ["Marcatura", "Passaggio"], ["Tecnica", "Marcatura"]]
 
     for pairidx, pair in enumerate(pairs_to_focus):
         # We only take the two corresponding features
@@ -145,14 +151,14 @@ def multiplot(
     plt.suptitle("Decision surface of decision trees trained on pairs of features on the Players dataset")
     plt.legend(loc="lower right", borderpad=0, handletextpad=0)
     _ = plt.axis("tight")
+    plt.savefig(os.path.join(os.getcwd(), "output", "Multi-plot.png"))
 
 
 def main():
     monoplot()
-    #multiplot()
+    multiplot()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-    multiplot()
 
